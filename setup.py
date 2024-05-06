c34abfd97854406a25cd57bedbc59d75e817198b
Dercilio <38219914+GGontijo@users.noreply.github.com>
2024-05-06 09:19:58 -0400

    adding long_description

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='fastapi_elasticsearch_middleware', 
     version='0.1.0', 
     url='https://github.com/GGontijo/fastapi-elasticsearch-middleware.git',
-    description='Elasticsearch Logger Middleware for FastAPI', 
+    description='Elasticsearch Logger Middleware for FastAPI',
+    long_description='Elasticsearch Logger Middleware for FastAPI' 
     author='Gabriel Gontijo', 
     author_email='gabrieldercilio08@gmail.com', 
     packages=find_packages(), 
