6b2df1ff5fac9ee3e9c2b186a22c9d0dddd488b4
Dercilio <38219914+GGontijo@users.noreply.github.com>
2024-05-06 08:59:05 -0400

    including package setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+  
+setup( 
+    name='fastapi_elasticsearch_middleware', 
+    version='0.1.0', 
+    url='https://github.com/GGontijo/fastapi-elasticsearch-middleware.git',
+    description='Elasticsearch Logger Middleware for FastAPI', 
+    author='Gabriel Gontijo', 
+    author_email='gabrieldercilio08@gmail.com', 
+    packages=find_packages(), 
+    install_requires=[ 
+        'fastapi', 
+        'elasticsearch' 
+    ], 
+) 
