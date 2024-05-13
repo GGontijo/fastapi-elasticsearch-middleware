a2499e0506b8f76df347876be712819a8637f96b
Dercilio <38219914+GGontijo@users.noreply.github.com>
2024-05-13 11:59:03 -0400

    package name changing

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
   
 setup( 
-    name='fastapi_elasticsearch_middleware', 
+    name='fastapi_elasticsearch', 
     version='0.1.0', 
-    url='https://github.com/GGontijo/fastapi-elasticsearch-middleware.git',
+    url='https://github.com/GGontijo/fastapi-elasticsearch.git',
     description='Elasticsearch Logger Middleware for FastAPI',
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
