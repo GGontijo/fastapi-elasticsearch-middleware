93457649a73be57645c27a67227b41e311068d0f
Dercilio <38219914+GGontijo@users.noreply.github.com>
2024-05-13 12:30:28 -0400

    new version released: 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
   
 setup( 
     name='fastapi_elasticsearch_middleware', 
-    version='0.1.0', 
+    version='1.0.0', 
     url='https://github.com/GGontijo/fastapi-elasticsearch-middleware.git',
     description='Elasticsearch Logger Middleware for FastAPI',
     long_description_content_type='text/markdown',
